aa546bc821c11a215e44d1f2c64a4c019c8150a1
Jakob Kruse <contact@jakob-kruse.de>
2023-12-03 00:33:29 +0100

    day 1

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,70 @@
-import { createStorage } from "unstorage";
-import fsLiteDriver from "unstorage/drivers/fs-lite";
-import redisDriver from "unstorage/drivers/redis";
+import console, { log } from "console";
 import fs from "fs/promises";
-import { FSFileAdapter } from "./file-retriever";
+
+function tap<T>(d: T) {
+  console.log(d);
+  return d;
+}
+
+function part1(inputLines: string[]) {
+  return inputLines
+    .map((line) => line.split(""))
+    .map((chars) => chars.map(Number))
+    .map((numChars) => numChars.filter((n) => !isNaN(n)))
+    .map((nums) => [nums[0], nums[nums.length - 1]])
+    .map((firstLast) => firstLast.join(""))
+    .map((str) => parseInt(str))
+    .reduce((sum, num) => sum + num, 0);
+}
+
+function part2(inputLines: string[]) {
+  const numberLiterals = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  let res: number[][] = [];
+  for (let line of inputLines) {
+    console.log(line);
+    let nums: number[] = [];
+    while (line.length > 0) {
+      for (const lit of numberLiterals) {
+        if (line.startsWith(lit)) {
+          nums.push(numberLiterals.indexOf(lit) + 1);
+        }
+      }
+
+      if (!isNaN(Number(line[0]))) {
+        nums.push(Number(line[0]));
+      }
+      line = line.substring(1);
+    }
+
+    res.push(nums);
+  }
+
+  return part1(res.map((r) => r.join("")));
+}
 
 async function run() {
-  const fsCache = createStorage({
-    driver: fsLiteDriver({ base: ".tmp" }),
-  });
+  const inputLines = (await fs.readFile("input.txt", "utf-8"))
+    .split("\n")
+    .filter(Boolean);
 
-  const redisCache = createStorage({
-    driver: redisDriver({}),
-  });
+  const result1 = part1(inputLines);
 
-  const fr = new FSFileAdapter(redisCache);
+  console.log(`Part 1: ${result1}`);
 
-  await fr.writeFile("test.txt", Buffer.from("Hello !"), {
-    type: "text",
-  });
+  const result2 = part2(inputLines);
 
-  const [file, meta] = await fr.readFile("test.txt");
-
-  console.log("Read Text File", file.toString(), "with meta:", meta);
-
-  const image = await fs.readFile("image.jpg");
-
-  await fr.writeFile("image.jpg", image, {
-    type: "image",
-  });
-
-  const [file2, meta2] = await fr.readFile("image.jpg");
-
-  console.log("Read Image File", "with meta:", meta2);
-
-  await fr.moveFile("image.jpg", "image2.jpg", {
-    type: "image",
-  });
-
-  const [file3, meta3] = await fr.readFile("image2.jpg");
-
-  console.log("Read Image File", "with meta:", meta3);
+  console.log(`Part 2: ${result2}`);
 }
 
 run();
